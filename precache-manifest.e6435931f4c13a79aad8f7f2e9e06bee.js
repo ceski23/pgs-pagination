9db4645bd0e50ab3efb578bd3f8476c0cf815799
Cezary Bober <ceski23@gmail.com>
2019-04-11 17:12:27 +0200

    Deployment

---
+++ precache-manifest.e6435931f4c13a79aad8f7f2e9e06bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc0f4b359798f9c11588",
+    "url": "/pgs-pagination/static/css/main.945aa46c.chunk.css"
+  },
+  {
+    "revision": "dc0f4b359798f9c11588",
+    "url": "/pgs-pagination/static/js/main.aa0b4603.chunk.js"
+  },
+  {
+    "revision": "21a4fd2d3ec04da6323c",
+    "url": "/pgs-pagination/static/js/runtime~main.0d764e43.js"
+  },
+  {
+    "revision": "14dc54cd3d584b7763ab",
+    "url": "/pgs-pagination/static/js/2.62ce01f3.chunk.js"
+  },
+  {
+    "revision": "9e495a61995fe2a05107e1a38ef5cca8",
+    "url": "/pgs-pagination/static/media/pgslogo.9e495a61.png"
+  },
+  {
+    "revision": "fddaed97fb0c8d41488eb81588d8a704",
+    "url": "/pgs-pagination/index.html"
+  }
+];
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.querySelector('#root'));
-serviceWorker.unregister();
